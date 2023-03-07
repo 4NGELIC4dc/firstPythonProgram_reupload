bc79e69356f27bd0b849033207e5fe9029da3843
Angelica Dela Casa <angelica020403@gmail.com>
2023-03-07 09:46:18 +0800

    Initial commit

---
+++ firstDemo.py
@@ -0,0 +1,12 @@
+username=input("Enter username:")
+username
+print(username)
+
+age=input("Enter age:")
+age
+print(age)
+
+first=int(input("Enter the first number:"))
+second=int(input("Enter the second number:"))
+print("The username is", username ,"and the age is", age)
+print("The product is", first*second)
